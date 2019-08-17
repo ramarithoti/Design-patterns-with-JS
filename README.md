aa4f6fd704d76690cdcbee26b1759688cd0389c4
Arithoti <arithoti@Anulekhas-MacBook-Pro.local>
2019-08-17 12:33:22 +0530

    design patterns

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Design-patterns-with-JS
 Popular design patterns with javascript
+
+Modules (code blocks) are designed so that we can get benifits like reusebility,composibility,organization,
+leverage,isolation
+
+# Modules mainly consits of 
+                         (1) -> Dependencies (imports or requires),
+                         (2) -> Implementation code 
+                         (3) -> Interface (exports or module.exports)
---
+++ modules.js
@@ -0,0 +1,41 @@
+
+// Design Patterns
+
+// Constructor Pattern
+
+// function Person(name,age,coder){
+//     this.name = name;
+//     this.age = age;
+//     this.coder = coder;
+// }
+// Person.prototype.info = function(){
+//     this.coder ?console.log(`${this.name} can code`):console.log(`${this.name} can't code`);
+// }
+
+// const p1 = new Person('Ram',28,true);
+// const p2 = new Person('Sam',26);
+
+// p1.info();
+// p2.info();
+
+// class Person{
+//     constructor(name,age,coder){
+//         this.name = name;
+//         this.age = age;
+//         this.coder = coder;
+//     }
+//     info(){
+//         this.coder?console.log(`${this.name} can code`):console.log(`${this.name} can't code`);
+//     }
+// }
+// const p1 = new Person('Ram',28,true);
+// const p2 = new Person('Sam',26);
+
+// p1.info();
+// p2.info();
+
+// // singleton pattern
+
+// class App {
+    
+// }
